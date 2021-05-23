507cbd24a241877365e90b666ba1e766392e955b
David Gray <dogray77@gmail.com>
2021-05-23 14:19:16 -0600

    create add todos reducer action

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import TodosContext from '../context'
 
 export default function TodoForm() {
+
+  const [todo, setTodo] = useState("")
+  const {dispatch} = useContext(TodosContext)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    dispatch({ type: "ADD_TODO", payload: todo })
+    setTodo("")
+  }
+
   return (
-    <form className="flex justify-center p-5">
+    <form 
+      onSubmit={handleSubmit}
+      className="flex justify-center p-5">
       <input 
         type="text"
-        className="border-black border-solid border-2" />
+        className="border-black border-solid border-2"
+        onChange={event => setTodo(event.target.value)}
+        value={todo}
+        />
     </form>
   )
 }
