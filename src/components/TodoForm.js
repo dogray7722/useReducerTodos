e0fa3a33f12d1ecdd480a9ef01f4f47f5d8cae80
David Gray <dogray77@gmail.com>
2021-05-23 15:49:00 -0600

    improve deletion behavior

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -9,6 +9,8 @@ export default function TodoForm() {
   useEffect(() => {
     if (currentTodo.text) {
       setTodo(currentTodo.text)
+    } else {
+      setTodo("")
     }
   }, [currentTodo.id])
 
